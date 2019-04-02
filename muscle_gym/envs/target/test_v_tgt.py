94f0a5f0f3e23a4b68cf692b710301379b10c671
smsong <ssm0445@gmail.com>
2019-04-02 00:09:53 -0700

    [.../target] get_vtgt_field_local in prog

--- muscle_gym/envs/target/test_v_tgt.py
+++ muscle_gym/envs/target/test_v_tgt.py
@@ -4,42 +4,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 # --------------------------------------------------------------------
 
-...
+#...
 
 # --------------------------------------------------------------------
 #rng_xy = np.array([[-30, 30], [-30, 30]])
 rng_xy = np.array([[-10, 10], [-5, 5]])
 vtgt_obj = VTgtField(rng_xy)
+
+plt.figure(0)
 plt.axes([.025, .025, .95, .95])
-X = vtgt_obj.grid[0]
-Y = vtgt_obj.grid[1]
-U = vtgt_obj.vect[0]
-V = vtgt_obj.vect[1]
+X = vtgt_obj.map[0]
+Y = vtgt_obj.map[1]
+U = vtgt_obj.vtgt[0]
+V = vtgt_obj.vtgt[1]
 R = np.sqrt(U**2 + V**2)
 plt.quiver(X, Y, U, V, R)
 plt.axis('equal')
-plt.show()
 
-p_sink = [3.8,2.5] # [x, y]
+p_sink = np.array([3.8,2.5]) # [x, y]
 d_sink = np.linalg.norm(p_sink)
-v_amp_rng = [1.0, 2.0]
+v_amp_rng = np.array([1.0, 2.0])
 vtgt_obj.create_vtgt_sink(p_sink, d_sink, v_amp_rng, v_phase0=np.pi)
 
+plt.figure(1)
 plt.axes([.025, .025, .95, .95])
-X = vtgt_obj.grid[0]
-Y = vtgt_obj.grid[1]
-U = vtgt_obj.vect[0]
-V = vtgt_obj.vect[1]
+X = vtgt_obj.map[0]
+Y = vtgt_obj.map[1]
+U = vtgt_obj.vtgt[0]
+V = vtgt_obj.vtgt[1]
+R = np.sqrt(U**2 + V**2)
+plt.quiver(X, Y, U, V, R)
+plt.axis('equal')
+
+
+pose = [3.8, 2.5, 30*np.pi/180] # [x, y, theta]
+vtgt = vtgt_obj.get_vtgt(pose[0:2])
+
+print('vtgt: {}'.format(vtgt))
+
+vtgt_field_local = vtgt_obj.get_vtgt_field_local(pose)
+
+plt.figure(2)
+plt.axes([.025, .025, .95, .95])
+X, Y = np.mgrid[-20:21, -10:11]/2
+U = vtgt_field_local[0]
+V = vtgt_field_local[1]
 R = np.sqrt(U**2 + V**2)
 plt.quiver(X, Y, U, V, R)
 plt.axis('equal')
 plt.show()
 
+
 import pdb; pdb.set_trace()
-
-
-pose = [0, 0, 0] # [x, y, theta]
-vtgt_obj.query_vtgt(pose)
-
 
 # --------------------------------------------------------------------
--- muscle_gym/envs/target/v_tgt_field.py
+++ muscle_gym/envs/target/v_tgt_field.py
@@ -5,41 +5,90 @@
 
 from __future__ import division # '/' always means non-truncating division
 import numpy as np
+from scipy import interpolate
 
 
 class VTgtField(object):
-    res = 2
+    res_map = 2
+    res_get = 2
+    rng_get = np.array([[-10, 10], [-5, 5]])
 
 # -----------------------------------------------------------------------------------------------------------------
     def __init__(self, rng_xy=np.array([[-10, 10], [-10, 10]])):
-        # origins and vectors
-        self.grid = self._generate_grid(rng_xy, self.res)
-        self.vect = -1*self.grid
+        # map coordinate and vtgt
+        self.create_map(rng_xy)
+        self.vtgt = -1*self.map
 
 # -----------------------------------------------------------------------------------------------------------------
     def __del__(self):
         nn = "empty"
 
 # -----------------------------------------------------------------------------------------------------------------
+    def create_map(self, rng_xy):
+        self.map_rng_xy = rng_xy
+        self.map = self._generate_grid(rng_xy, self.res_map)
+
+# -----------------------------------------------------------------------------------------------------------------
     def create_vtgt_sink(self, p_sink, d_sink, v_amp_rng, v_phase0=np.random.uniform(-np.pi, np.pi)):
-        # set vector orientations
-        rng_xy = np.array([ -p_sink[0] + np.array([self.grid[0,0,0], self.grid[0,-1,0]]), 
-                            -p_sink[1] + np.array([self.grid[1,0,0], self.grid[1,0,-1]])])
-        self.vect = -self._generate_grid(rng_xy, self.res)
+        # set vtgt orientations
+        rng_xy = (-p_sink + self.map_rng_xy.T).T
+        self.vtgt = -self._generate_grid(rng_xy, self.res_map)
 
-        # set vector amplitudes
-        self._set_sink_vect_amp(p_sink, d_sink, v_amp_rng, v_phase0)
+        # set vtgt amplitudes
+        self._set_sink_vtgt_amp(p_sink, d_sink, v_amp_rng, v_phase0)
 
-# -----------------------------------------------------------------------------------------------------------------
-    def query_vtgt(self, pose):
-        return
+        self.vtgt_interp_x = interpolate.interp2d(self.map[0,:,0], self.map[1,0,:], self.vtgt[0].T, kind='linear')
+        self.vtgt_interp_y = interpolate.interp2d(self.map[0,:,0], self.map[1,0,:], self.vtgt[1].T, kind='linear')
 
 # -----------------------------------------------------------------------------------------------------------------
-    def query_v_global(self, pose):
-        return
+    def get_vtgt_field_local(self, pose):
+        xy = pose[0:2]
+        th = pose[2]
+
+        # create query map
+        get_rng_xy0 = (xy + self.rng_get.T).T
+        get_map0 = self._generate_grid(get_rng_xy0, self.res_get)
+        get_map_x = np.cos(th)*get_map0[0,:,:] - np.sin(th)*get_map0[1,:,:]
+        get_map_y = np.sin(th)*get_map0[0,:,:] + np.cos(th)*get_map0[1,:,:]
+-> rotate around center
+        # get vtgt
+        vtgt_x0 = np.reshape(np.array([self.vtgt_interp_x(x, y) \
+                            for x, y in zip(get_map_x.flatten(), get_map_y.flatten())]),
+                            get_map_x.shape)
+        vtgt_y0 = np.reshape(np.array([self.vtgt_interp_y(x, y) \
+                            for x, y in zip(get_map_x.flatten(), get_map_y.flatten())]),
+                            get_map_x.shape)
+
+        vtgt_x = np.cos(th)*vtgt_x0 - np.sin(th)*vtgt_y0
+        vtgt_y = np.sin(th)*vtgt_x0 + np.cos(th)*vtgt_y0
+
+        # debug
+        import matplotlib.pyplot as plt
+        plt.figure(100)
+        plt.axes([.025, .025, .95, .95])
+        plt.plot(get_map_x, get_map_y, '.')
+        plt.axis('equal')
+
+        plt.figure(101)
+        plt.axes([.025, .025, .95, .95])
+        R = np.sqrt(vtgt_x0**2 + vtgt_y0**2)
+        plt.quiver(get_map_x, get_map_y, vtgt_x0, vtgt_y0, R)
+        plt.axis('equal')
+
+        plt.show()
+
+        import pdb; pdb.set_trace()
+
+        return np.stack((vtgt_x, vtgt_y))
 
 # -----------------------------------------------------------------------------------------------------------------
-    def _generate_grid(self, rng_xy=np.array([[-10, 10], [-10, 10]]), res=.5):        
+    def get_vtgt(self, xy):
+        vtgt_x = self.vtgt_interp_x(xy[0], xy[1])
+        vtgt_y = self.vtgt_interp_y(xy[0], xy[1])
+        return np.array([vtgt_x, vtgt_y])
+
+# -----------------------------------------------------------------------------------------------------------------
+    def _generate_grid(self, rng_xy=np.array([[-10, 10], [-10, 10]]), res=2):
         xo = .5*(rng_xy[0,0]+rng_xy[0,1])
         x_del = (rng_xy[0,1]-xo)*res
         yo = .5*(rng_xy[1,0]+rng_xy[1,1])
@@ -50,11 +99,11 @@ class VTgtField(object):
         return grid
 
 # -----------------------------------------------------------------------------------------------------------------
-    def _set_sink_vect_amp(self, p_sink, d_sink, v_amp_rng, v_phase0, d_dec = 1):
+    def _set_sink_vtgt_amp(self, p_sink, d_sink, v_amp_rng, v_phase0, d_dec = 1):
         # d_dec: start to decelerate within d_dec of sink
 
-        for i_x, x in enumerate(self.grid[0,:,0]):
-            for i_y, y in enumerate(self.grid[1,0,:]):
+        for i_x, x in enumerate(self.map[0,:,0]):
+            for i_y, y in enumerate(self.map[1,0,:]):
                 d = np.linalg.norm([ x-p_sink[0], y-p_sink[1] ])
                 if d > d_sink + d_dec:
                     v_amp = v_amp_rng[1]
@@ -66,12 +115,7 @@ class VTgtField(object):
                     v_amp0 = .5*np.diff(v_amp_rng)*np.sin(v_phase) + np.mean(v_amp_rng)
                     v_amp = d*v_amp0
 
-                #import pdb; pdb.set_trace()
-                amp_norm = np.linalg.norm(self.vect[:,i_x,i_y])
-                self.vect[0,i_x,i_y] = v_amp*self.vect[0,i_x,i_y]/amp_norm
-                self.vect[1,i_x,i_y] = v_amp*self.vect[1,i_x,i_y]/amp_norm
-
-                #import pdb; pdb.set_trace()
-
-
+                amp_norm = np.linalg.norm(self.vtgt[:,i_x,i_y])
+                self.vtgt[0,i_x,i_y] = v_amp*self.vtgt[0,i_x,i_y]/amp_norm
+                self.vtgt[1,i_x,i_y] = v_amp*self.vtgt[1,i_x,i_y]/amp_norm
         
